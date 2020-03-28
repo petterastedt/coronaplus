12a88e22478975b58b372bd79ab2c4f89aae6549
petterastedt <petter.astedt@gmail.com>
2020-03-28 19:21:50 +0100

    Updates

---
+++ precache-manifest.18b41d9d6be732063a2e7872691a7ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3033e571ad72c63b15e87f42f9126759",
+    "url": "/coronaplus/index.html"
+  },
+  {
+    "revision": "da470d5d253614cd0dbc",
+    "url": "/coronaplus/static/css/main.2155459c.chunk.css"
+  },
+  {
+    "revision": "bd324cfecaaec8886f71",
+    "url": "/coronaplus/static/js/2.fafaa4f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coronaplus/static/js/2.fafaa4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da470d5d253614cd0dbc",
+    "url": "/coronaplus/static/js/main.6d19e2ef.chunk.js"
+  },
+  {
+    "revision": "780ef313a48b36d6b0e8",
+    "url": "/coronaplus/static/js/runtime-main.98827ee8.js"
+  }
+]);
